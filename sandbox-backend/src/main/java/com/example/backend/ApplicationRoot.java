edafde3a746c080c89fa5e012baccd41ebee8ec0
Jiri Nemec <jiri.nemec@gmail.com>
2023-06-05 15:28:40 +0200

    Initial

---
+++ sandbox-backend/src/main/java/com/example/backend/ApplicationRoot.java
@@ -0,0 +1,25 @@
+package com.example.backend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync
+@SpringBootApplication
+@EnableScheduling
+public class ApplicationRoot implements ApplicationRunner {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationRoot.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationRoot.class, args);
+  }
+
+  @Override
+  public void run(ApplicationArguments args) {}
+}
---
+++ sandbox-commons/src/main/java/com/example/commons/Utilities.java
@@ -0,0 +1,12 @@
+package com.example.commons;
+
+public final class Utilities {
+
+    private Utilities() {
+    }
+
+    public static String getHello(String name) {
+        return "Hello " + name + "!";
+    }
+
+}
